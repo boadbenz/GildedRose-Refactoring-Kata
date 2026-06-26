34a7cec3ce26832fa0e092d85b704d5332494938
boadbenz <manaya002@hotmail.com>
2026-06-26 15:16:07 +0700

    refactor: completed Product Factory

--- Java/src/main/java/com/gildedrose/oop/Product/factory/ProductFactory.java
+++ Java/src/main/java/com/gildedrose/oop/Product/factory/ProductFactory.java
@@ -1,15 +1,23 @@
 package com.gildedrose.oop.Product.factory;
 
-import com.gildedrose.oop.Product.Product;
+import com.gildedrose.oop.Product.*;
 
 public class ProductFactory {
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String BACKSTAGE_PASSED = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
-    private String AGED_BRIE = "Aged Brie";
-    private String BACKSTAGE_PASSED = "Backstage passes to a TAFKAL80ETC concert";
-    private String SULFURAS = "Sulfuras, Hand of Ragnaros";
-
-    public static Product getProduct(String productName) {
-        return null;
+    public static Product getProduct(String productName, int quality, int sellIn) {
+        switch (productName) {
+            case AGED_BRIE:
+                return new AgedBrieImpl(quality, sellIn);
+            case BACKSTAGE_PASSED:
+                return new BackstagePassesImpl(quality, sellIn);
+            case SULFURAS:
+                return new SulfurasImpl(quality, sellIn);
+            default:
+                return new CommonProductImpl(quality, sellIn);
+        }
     }
 
 }
